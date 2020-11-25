4d1f47058b6e2fd658667c49ef6d1c8699635904
youngmi97 <jeong7331@kaist.ac.kr>
2020-11-25 16:46:51 +0900

    first page no resp

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,72 +1,17 @@
 import "./App.css";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
+
 import Main from "./pages/Main";
 
-const useStyles = makeStyles((theme) => ({
-  bigGrid: {},
-  pageMockUp: {
-    float: "right",
-    position: "absolute",
-    zIndex: -1,
-  },
-  phoneMockUp: {
-    float: "right",
-    marginTop: "200px",
-    position: "absolute",
-    zIndex: 1,
-  },
-  mockUpGrid: {},
-  imgDiv: { width: "100%", position: "relative" },
-}));
-
 function App() {
-  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Main /> */}
-        <img src="/topLogo.svg"></img>
-        <Grid
-          container
-          className={classes.bigGrid}
-          direction="row"
-          justify="center"
-          alignItems="center"
-          xs={12}
-        >
-          <Grid
-            container
-            className={classes.mockUpGrid}
-            direction="row"
-            justify="center"
-            alignItems="center"
-            xs={6}
-          >
-            Hello
-          </Grid>
-          <Grid
-            container
-            className={classes.mockUpGrid}
-            direction="row"
-            justify="center"
-            alignItems="center"
-            xs={6}
-          >
-            <div className={classes.imgDiv}>
-              <img
-                className={classes.pageMockUp}
-                src="/landingPageMockUp.svg"
-                alt="mockPage"
-              ></img>
-              <img
-                className={classes.phoneMockUp}
-                src="/phoneMockUp.svg"
-                alt="mockPhone"
-              ></img>
-            </div>
-          </Grid>
-        </Grid>
+        <img
+          style={{ position: "fixed", top: "0px" }}
+          src="/topLogo.svg"
+          alt="logo"
+        ></img>
+        <Main />
       </header>
     </div>
   );
--- landing-page/src/pages/Main.js
+++ landing-page/src/pages/Main.js
@@ -1,22 +1,97 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import { Grid } from "@material-ui/core";
+import "../App.css";
 
 const useStyles = makeStyles((theme) => ({
-  bigGrid: {},
+  textField: {
+    background: "#ffffff",
+    width: "366px",
+    height: "56px",
+    borderRadius: "4px",
+    border: "1px solid #EFEFF4",
+    boxSizing: "border-box",
+  },
+  startButton: {
+    marginLeft: "16px",
+    height: "56px",
+    width: "133px",
+    background: "#6600FF;",
+    boxShadow: "8px 16px 40px rgba(42, 0, 77, 0.3)",
+    borderRadius: "6px",
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "12px",
+    lineHeight: "17px",
+    /* identical to box height */
+
+    textAlign: "center",
+    letterSpacing: "0.03em",
+    textTransform: "uppercase",
+  },
+  bigGrid: { width: "100%" },
+  bigText: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "72px",
+    lineHeight: "88px",
+    /* or 122% */
+    letterSpacing: "0.02em",
+    marginLeft: "100px",
+  },
+  medText: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    width: "100%",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "24px",
+    lineHeight: "29px",
+    marginTop: "35px",
+    marginLeft: "100px",
+  },
+  submitInfo: {
+    width: "100%",
+    height: "100px",
+  },
+  submitInfo2: {
+    marginTop: "35px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    verticalAlign: "middle",
+    marginLeft: "100px",
+  },
   pageMockUp: {
     float: "right",
+    position: "fixed",
+    top: "80px",
+    right: "0px",
+  },
+  textHolder: {
+    width: "100%",
   },
   phoneMockUp: {
     float: "right",
+    position: "fixed",
+    top: "210px",
+    right: "365px",
   },
-  mockUpGrid: {},
+  mockUpGrid: {
+    width: "100%",
+  },
 }));
 
 function Main() {
   const classes = useStyles();
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <Grid
         container
         className={classes.bigGrid}
@@ -25,9 +100,38 @@ function Main() {
         alignItems="center"
         xs={12}
       >
-        <Grid item direction="row" justify="center" alignItems="center" xs={6}>
-          {" "}
-          Hello{" "}
+        <Grid item className={classes.textHolder} direction="column" xs={6}>
+          <div className={classes.bigText}>
+            <div>Subscriptions</div>
+            <div>made easy</div>
+          </div>
+          <div className={classes.medText}>
+            <div>Add, make changes and cancel</div>
+            <div>subscriptions in one place</div>
+          </div>
+          <div className={classes.submitInfo}>
+            <Grid
+              item
+              className={classes.submitInfo2}
+              direction="row"
+              xs={12}
+              spacing={3}
+            >
+              <TextField
+                className={classes.textField}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+              />
+              <Button
+                className={classes.startButton}
+                variant="contained"
+                color="primary"
+              >
+                GET STARTED
+              </Button>
+            </Grid>
+          </div>
         </Grid>
         <Grid
           container
@@ -39,7 +143,7 @@ function Main() {
         >
           <img
             className={classes.pageMockUp}
-            src="/landingPageMockUp.svg"
+            src="/webMockUp.svg"
             alt="mockPage"
           ></img>
           <img
